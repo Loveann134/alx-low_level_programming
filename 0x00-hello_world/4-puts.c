6cd571eddd5c08bd6d43c890a9bb8e3415d62222
Loveann134 <akingbuleprecious@gmail.com>
2022-09-08 15:46:56 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,5 @@
  * Return: 0 if successful
  */
 int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
+{puts("\"Programming is like building a multilingual puzzle");
+return (0);}
