1c33b896b7ab44b5ad1f03ce1a7dc3492162fc7e
Loveann134 <akingbuleprecious@gmail.com>
2022-09-08 15:19:55 -0700

    4-puts.c

---
+++ 4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <studio.h>
+
+/**
+ * main - Printing with c
+ *
+ * Return: Always 0 (Sucessful)
+ */
+int main(void)
+{puts ("\"Programming is like builing a multilingual puzzle");
+	return (0)}
